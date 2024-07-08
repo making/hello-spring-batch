4d3f53c6ba7c7168f68f134e1e900c6593df662c
Toshiaki Maki <makingx@gmail.com>
2024-07-08 16:59:38 +0900

    first commit

---
+++ src/main/java/org/example/hellospringbatch/Person.java
@@ -0,0 +1,5 @@
+package org.example.hellospringbatch;
+
+public record Person(String firstName, String lastName) {
+
+}
---
+++ src/test/java/org/example/hellospringbatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.hellospringbatch;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
